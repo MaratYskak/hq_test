88990db6a4a3ab035d00e506691047c1cb364be8
MaratYskak <yskakmarat2@gmail.com>
2023-09-20 21:56:53 +0600

    add UserLessonsView

--- hq_test_proj/hq_test_app/api/urls.py
+++ hq_test_proj/hq_test_app/api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from hq_test_app.api.views import UserViewSet
+from hq_test_app.api.views import UserViewSet, UserLessonsView
 
 router = SimpleRouter()
 router.register(r'users', UserViewSet, basename="users")
+router.register(r'lesson-status', UserLessonsView, basename="lesson-status")
 
 urlpatterns = router.urls
--- hq_test_proj/hq_test_app/api/views.py
+++ hq_test_proj/hq_test_app/api/views.py
@@ -2,6 +2,12 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
 from hq_test_app.api.serializers import UserRegistrationSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from hq_test_proj.hq_test_app.api.serializers import LessonSerializer
+
+from hq_test_proj.hq_test_app.models import Lesson
 
 
 class UserViewSet(CreateModelMixin, GenericViewSet):
@@ -10,3 +16,16 @@ class UserViewSet(CreateModelMixin, GenericViewSet):
 
     def perform_create(self, serializer):
         serializer.save()
+
+class UserLessonsView(APIView):
+    """
+    возвращает список уроков с информацией о статусе и времени просмотра для данного пользователя
+    """
+    def get(self, request):
+        user = request.user
+
+        user_lessons = Lesson.objects.filter(lessonstatus__user=user, lessonstatus__status='Просмотрено')
+
+        serializer = LessonSerializer(user_lessons, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
