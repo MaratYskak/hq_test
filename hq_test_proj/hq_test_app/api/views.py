f8965d90dba0e3761fd5ce0db83e3ed078f6ff9b
MaratYskak <yskakmarat2@gmail.com>
2023-09-20 21:40:00 +0600

    update UserViewSet

--- hq_test_proj/hq_test_app/api/views.py
+++ hq_test_proj/hq_test_app/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import GenericViewSet 
 from rest_framework.mixins import CreateModelMixin
 from hq_test_app.api.serializers import UserRegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(CreateModelMixin, GenericViewSet):
     serializer_class = UserRegistrationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
